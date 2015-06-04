f4ef95c4a45812fb536cb34dce96422cf8a0c63f
Aakarsh Singh <aakarsh7@ymail.com>
2015-06-04 20:59:56 +0530

    Competed iteration structure

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main
 {
+    static String directoryPath;
+    static int reNameCount=0;
     public static void main(String[] args) throws IOException
     {
-        String s=args[0];
-        Files.walk(Paths.get(s)).forEach(filePath ->
+        directoryPath=args[0];
+
+        Files.walk(Paths.get(directoryPath)).forEach(filePath ->
         {
             if (Files.isRegularFile(filePath))
             {
-                System.out.println(filePath);
+                String fileName=filePath.getFileName().toString();
+                int indexOfLastPeriod=fileName.lastIndexOf('.');
+                String extension=fileName.substring(indexOfLastPeriod+1);
+                if(extension.equalsIgnoreCase("srt"))
+                {
+                    int indexOfFirstPeriod=fileName.indexOf('.');
+                    String newFileName=fileName.substring(0,indexOfFirstPeriod+1)+extension;
+                    if(reNameFunction(fileName, newFileName))
+                    {
+                        incrementCount();
+                    }
+                }
             }
         });
-        System.out.println(s);
+        System.out.println(reNameCount+" files renamed");
+    }
+
+    static boolean reNameFunction(String fileName, String newFileName)
+    {
+        return false;
+    }
+
+    static void incrementCount()
+    {
+        reNameCount++;
     }
 }
